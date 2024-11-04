ee2f50f65341968f05fb3c57de2d699d16900a82
Zack Freedman <ztfreedman@gmail.com>
2024-11-03 22:59:28 -0500

    Mason auto sets up lsps selected

--- nvim-config/lsp/mason.lua
+++ nvim-config/lsp/mason.lua
@@ -11,11 +11,10 @@ local servers = {
 
 local settings = {
 	ui = {
-		border = "none",
 		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
 		},
 	},
 	log_level = vim.log.levels.INFO,
@@ -35,18 +34,19 @@ if not lspconfig_status_ok then
 	return
 end
 
+require("mason-lspconfig").setup_handlers {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function (server_name) -- default handler
+		local opts = {}
+		local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server_name)
+		if require_ok then
+			opts = vim.tbl_deep_extend("force", conf_opts, opts)
+		end
+		lspconfig[server_name].setup(opts)
+	end,
+}
 
-local opts = {}
-
-for _, server in pairs(servers) do
-	server = vim.split(server, "@")[1]
-
-	local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend("force", conf_opts, opts)
-	end
-
-	lspconfig[server].setup(opts)
-end
 
 
